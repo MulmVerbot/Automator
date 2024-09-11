9450c55321d8403285f3ff35a8a4d226301f5d2b
MulmVerbot <70117614+MulmVerbot@users.noreply.github.com>
2024-09-11 13:57:28 +0200

    0.0
    wegen bug mit einem Button der nicht auftauchte + ich von .pack() keine Ahnung habe. Hab ich das Grid mal schnell auf .place() umgeswitched
    - ein paar Bugfixes

--- main.py
+++ main.py
@@ -14,30 +14,40 @@ class Automator:
         self.bildschirm_breite, self.bildschirm_hoehe = g3.size()
         self.anzahl_der_clicks = 0
         self.master.title(self.Programm_Name + " " + self.Version)
+    ## andere Vars
+        self.gespeicherte_Aufzeichnungen = None
+        self.verzögerung = 0.1
+    ## andere Vars ENDE
         self.GUI_laden()
 
     def GUI_laden(self):
         print("[-GUI laden-] Gestartet")
         self.aufz_start_knopp = tk.Button(root, text="Aufzeichnung starten", command=self.Aufzeichnung_starten_vor)
-        self.aufz_start_knopp.pack(side="bottom", padx=20, pady=1, expand=True)
+        self.aufz_start_knopp.place(x=10,y=40)
         self.aufz_start_l = tk.Label(root, text="Mausclicks aufzeichnen")
-        self.aufz_start_l.pack(side="bottom", padx=10, pady=1, expand=True)
+        self.aufz_start_l.place(x=10,y=10)
         self.Aufzeichnung_abspielen_maus_knopp = tk.Button(root, text="Aufzeichnung abspielen", command=self.Aufzeichnung_abspielen_maus)
-        self.Aufzeichnung_abspielen_maus_knopp.pack(side="left", padx=10, pady=1, expand=True)
-        self.ausgabe_mausklicks = tk.Text(root, width="260", height="420")
-        self.ausgabe_mausklicks.pack(side="left", padx=10, pady=1, expand=True)
+        self.Aufzeichnung_abspielen_maus_knopp.place(x=10,y=70)
+        self.ausgabe_mausklicks = tk.Text(root, width="60", height="40")
+        self.ausgabe_mausklicks.place(x=300, y=20)
         self.Mausaufzeichnung_zurücksetzen_knopp = tk.Button(root, text="Mausaufzeichnung zurücksetzen", command=self.Mausaufzeichnung_zurücksetzen)
-        self.Mausaufzeichnung_zurücksetzen_knopp.pack(side="left", padx=10, pady=10, expand=True)
+        self.Mausaufzeichnung_zurücksetzen_knopp.place(x=10,y=110)
+        self.ausgabe_mausklicks.configure(highlightthickness=0)
+        self.ausgabe_mausklicks.configure(state="disabled")
+
         print("[-GUI laden-] Beendet.")
 
     def Mausaufzeichnung_zurücksetzen(self):
         print("Mausaufzeichnung_zurücksetzen(def)")
         self.gespeicherte_Aufzeichnungen = []
+        self.anzahl_der_clicks = 0
+        self.ausgabe_mausklicks.configure(state="normal")
+        self.ausgabe_mausklicks.delete("1.0", tk.END)
+        self.ausgabe_mausklicks.configure(state="disabled")
 
 
     def Aufzeichnung_starten_vor(self):
-        verzögerung = 0.1
-        self.Maus_aufz_thread = threading.Timer(int(verzögerung), self.Aufzeichnung_starten)
+        self.Maus_aufz_thread = threading.Timer(int(self.verzögerung), self.Aufzeichnung_starten)
         self.Maus_aufz_thread.daemon = True
         self.Maus_aufz_thread.start()
         self.aufz_start_knopp.configure(text="Aufzeichnung stoppen", command=self.Aufzeichnung_thread_stopp)
@@ -48,10 +58,13 @@ class Automator:
 
         def on_click(x, y, button, pressed):
             if pressed:
+                self.ausgabe_mausklicks.configure(state="normal")
                 self.anzahl_der_clicks += 1
                 self.gespeicherte_Aufzeichnungen.append(f"{x},{y}")
                 self.ausgabe_mausklicks.insert(tk.END, f"{self.anzahl_der_clicks}. Mausklick Cursor-Koordinaten: X= {x} Y={y}\n")
                 print(self.gespeicherte_Aufzeichnungen)
+                self.ausgabe_mausklicks.see(tk.END)
+                self.ausgabe_mausklicks.configure(state="disabled")
 
         # Mausklick-Ereignisregistrierung in einem separaten Thread
         self.listener = mouse.Listener(on_click=on_click)
@@ -63,27 +76,30 @@ class Automator:
         if self.listener:
             self.listener.stop()  # Stoppt den listener damit das endlich mal hinhaut hier
             self.listener = None
+            self.anzahl_der_clicks = 0
         try:
             if self.Maus_aufz_thread:
                 self.Maus_aufz_thread.cancel()
+                self.anzahl_der_clicks = 0
                 print("[-Aufzeichnung-] Thread beendet")
         except Exception as e:
             print(f"[-Aufzeichnung-] Fehler beim Beenden des Threads: {e}")
 
     def Aufzeichnung_abspielen_maus(self):
         print("Aufzeichnung_abspielen_maus(def)")
-        Zeugs = self.gespeicherte_Aufzeichnungen
-        for M in Zeugs:
-            kord = M
-            x, y = map(float, kord.split(','))
-            g3.moveTo(x,y)
-            g3.click()
-            time.sleep(0.1)
-            print(f"Bewegt nach {kord}.")
+        if self.gespeicherte_Aufzeichnungen != None or "":
+            Zeugs = self.gespeicherte_Aufzeichnungen
+            for M in Zeugs:
+                kord = M
+                x, y = map(float, kord.split(','))
+                g3.moveTo(x,y)
+                g3.click()
+                time.sleep(0.1)
+                print(f"Bewegt nach {kord}.")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    width = 660
+    width = 820
     height = 420
     def mittig_fenster(root, width, height):
         fenster_breite = root.winfo_screenwidth()
